e3a6c8704c7fea2c6f3a1555f93b72921d1becd6
Jeremy Spofford <12678732-jeremyspofford@users.noreply.gitlab.com>
2026-01-02 13:10:21 -0500

    fix(terraform): remove local credentials to support CI

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,9 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region                   = "us-east-1"
-  shared_credentials_files = ["/Users/jeremyspofford/.aws/credentials"]
-  profile                  = "personal"
+  region = "us-east-1"
   default_tags {
     tags = {
       Project     = "Portfolio"
