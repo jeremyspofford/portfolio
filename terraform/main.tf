79f21cd32f5793c85cf5ffaf7da163f0ba44d40e
Jeremy Spofford <12678732-jeremyspofford@users.noreply.gitlab.com>
2026-01-02 13:23:41 -0500

    fix: configure s3 remote state and upgrade terraform version

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket       = "portfolio-tf-state-jeremyspofford"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
 }
 
 provider "aws" {
